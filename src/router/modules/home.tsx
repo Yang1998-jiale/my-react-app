dd01eed292207d8ac5d9b1e0ac45169d6e0c22e5
yjl <2018770090@qq.com>
2024-04-22 11:41:51 +0800

    提交了

--- src/router/modules/home.tsx
+++ src/router/modules/home.tsx
@@ -2,12 +2,14 @@
  * @Author: yjl
  * @Date: 2024-04-18 15:52:33
  * @LastEditors: yjl
- * @LastEditTime: 2024-04-19 11:36:36
+ * @LastEditTime: 2024-04-22 11:41:34
  * @Description: 描述
  */
 import { lazy } from "react";
 const Home = lazy(() => import("@/views/home/index"));
 // import Home from "@/views/home/index";
+
+
 
 const home: any[] = [
   {
