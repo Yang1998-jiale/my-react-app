d9bd6f8139580a8745edef20a6205ef5af0f527c
yjl <2018770090@qq.com>
2024-04-22 11:18:24 +0800

    feat: 🚀 新增czg  git提交工具

--- src/views/study/index.tsx
+++ src/views/study/index.tsx
@@ -1,19 +1,31 @@
-import { useState } from 'react';
-
+/*
+ * @Author: yjl
+ * @Date: 2024-04-19 09:52:45
+ * @LastEditors: yjl
+ * @LastEditTime: 2024-04-22 10:35:32
+ * @Description: 描述
+ */
+import { useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function Index() {
   const [state, setState] = useState({
-    message:'index',
+    message: "index",
     count: 1,
-  })
+  });
+  const location = useLocation();
   return (
     <>
-      <div
-        className='w-100% h-100% cursor-pointer'
-        onClick={() => setState({ ...state, count: state.count + 1 })}
-      >
-        {state.message + state.count}
-      </div>
+      {location.pathname !== "/study/index" ? (
+        <Outlet />
+      ) : (
+        <div
+          className="w-100% h-100% cursor-pointer"
+          onClick={() => setState({ ...state, count: state.count + 1 })}
+        >
+          {state.message + state.count}
+        </div>
+      )}
     </>
   );
 }
